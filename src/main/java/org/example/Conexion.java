b7c6e716cfeb8d0ee4d5a6f8cb349d5f9a021234
marimilhas <marimilhas@gmail.com>
2023-04-20 00:44:23 -0300

    Commit Nº3 E3

--- src/main/java/org/example/Conexion.java
+++ src/main/java/org/example/Conexion.java
@@ -8,29 +8,32 @@ public class Conexion {
     public void obtener_pronosticos(String nro_ronda){ //Debemos crear algun tipo de variable para almacenar los pronosticos y retornarla
         Statement stmt = null;
         ResultSet resultado = null;
+        boolean no_existe_ronda = true;
 
         try{
             cargar_clase();
-            Connection con = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/dbprueba2023",
-                    "userdb2023", "pass2023");
+            Connection con = DriverManager.getConnection("jdbc:mysql://db4free.net/basededatos2023",
+                    "marianamilhas", "dbcursojava2023");
             stmt = con.createStatement();
             resultado = stmt.executeQuery(
                     "SELECT * FROM pronostico INNER JOIN partido ON pronostico.id_partido = " +
                     "partido.id_partido WHERE partido.ronda = " + nro_ronda);
 
-            if (resultado.next()){
-                while (resultado.next()){
-                    String equipo1 = resultado.getString("equipo1");
-                    String equipo2 = resultado.getString("equipo2");
-                    int goles1 = resultado.getInt("cant_goles1");
-                    int goles2 = resultado.getInt("cant_goles2");
-                    int gana1 = resultado.getInt("gana1");
-                    int gana2 = resultado.getInt("gana2");
-                    Funciones.crear_pronostico(equipo1, equipo2, goles1, goles2, gana1, gana2);
-                }
-            } else {
+            while (resultado.next()){
+                no_existe_ronda = false;
+                String equipo1 = resultado.getString("equipo1");
+                String equipo2 = resultado.getString("equipo2");
+                int goles1 = resultado.getInt("cant_goles1");
+                int goles2 = resultado.getInt("cant_goles2");
+                int gana1 = resultado.getInt("gana1");
+                int gana2 = resultado.getInt("gana2");
+                Funciones.crear_pronostico(equipo1, equipo2, goles1, goles2, gana1, gana2);
+            }
+
+            if (no_existe_ronda){
                 System.out.println("Ya no hay más rondas disponibles, gracias por jugar!");
             }
+
         } catch (Exception e) {
             System.out.println("ERROR: " + e.getMessage());
         }
--- src/main/java/org/example/Partido.java
+++ src/main/java/org/example/Partido.java
@@ -2,19 +2,13 @@ package org.example;
 public class Partido {
     private Equipo equipo1;
     private Equipo equipo2;
-    public int golesEquipo1;
-    public int golesEquipo2;
+    private int golesEquipo1;
+    private int golesEquipo2;
 
     public Partido(Equipo equipo1, Equipo equipo2, int golesEquipo1, int golesEquipo2){
         this.equipo1 = equipo1;
         this.equipo2 = equipo2;
         this.golesEquipo1 = golesEquipo1;
         this.golesEquipo2 = golesEquipo2;
-    }
-    public Equipo getEquipo1() {
-        return equipo1;
-    }
-    public Equipo getEquipo2() {
-        return equipo2;
     }
 }
